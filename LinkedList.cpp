0e63f73923acfd5670a72fed975ff4d471109920
restuchan <restu.chan188@gmail.com>
2025-05-08 10:40:04 +0700

    jika duplikasi noMhs

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -43,4 +43,19 @@ class List
         Node *previous = START;
         Node *current = START;
 
-        
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    

23358911795ee6e9b22b0eb4b35ffd28bc6b1338
restuchan <restu.chan188@gmail.com>
2025-05-08 11:15:34 +0700

    menambahkan daftar isi

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -117,4 +117,18 @@ class List
         SingleLinkedList mhs;
         int nim;
         char ch;
-        
+        while (1)
+        {
+            cout << endl
+                 << "Menu";
+            cout << endl
+                 << "1. Menambahkan data kedalam list" << endl;
+            cout << "2. Menghapus data dari dalam list" << endl;
+            cout << "3. Menampilkan semua data didalam list" << endl;
+            cout << "4. Mencari data dalam list" << endl;
+            cout << "5. Keluar" << endl;
+            cout << endl
+                 << "Masukkan pilihan (1-5): ";
+            cin >> ch;
+            switch (ch)
+            

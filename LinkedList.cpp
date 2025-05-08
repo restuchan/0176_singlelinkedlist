5285b14c3feedc4d61278fbd61ccc2057074eeef
restuchan <restu.chan188@gmail.com>
2025-05-08 10:46:13 +0700

    menambahkan delete node

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -77,4 +77,19 @@ class List
         return (*current != NULL);
     }
 
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+        return false;
+
+        if (current == START)
+        START = START->next;
+        else
+        previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
     

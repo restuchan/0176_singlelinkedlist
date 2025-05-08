31a29486ad7ec8aaf94184ca79e54c7329bc7c39
restuchan <restu.chan188@gmail.com>
2025-05-08 11:16:29 +0700

    case 3

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -138,4 +138,23 @@ class List
             }
             break;
     
+            case '2':
+            {
+                if (mhs.listEmpty())
+                {
+                    cout << endl
+                         << "List kosong" << endl;
+                    break;
+                }
+                cout << endl
+                     << "\nMasukkan no mahasiswa yang akan dihapus : ";
+                cin >> nim;
+                if (mhs.delNode(nim) == false)
+                    cout << endl
+                         << "Data tidak ditemukan" << endl;
+                else
+                    cout << endl    
+                         << "Data dengan nomer mahasiswa " << nim << "berhasil dihapus " << endl;
+            }
+            break;
             

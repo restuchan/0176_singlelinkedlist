0ad27fdeb6e8df757e9895cf99013f722c89f205
restuchan <restu.chan188@gmail.com>
2025-05-08 09:52:56 +0700

    first commit

---
+++ LinkedList.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int noMhs;
+    Node *next;
+};
+
+class List
+{
+    Node *START;
+
+    public:
+}
